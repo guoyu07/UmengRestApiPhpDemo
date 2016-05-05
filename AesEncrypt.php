299a60d9c9db38ccb510af832d12cfa80b6ee570
inzaghi <46131242@163.com>
2016-05-05 16:54:05 +0800

    Update AesEncrypt.php

--- AesEncrypt.php
+++ AesEncrypt.php
@@ -53,15 +53,15 @@ class AesEncrypt{
 }
 
 
-$key = "273d7e70c2d115e62e0e45656ff82b39";
-$aes = new AesEncrypt($key);
+// $key = "273d7e70c2d115e62e0e45656ff82b39";
+// $aes = new AesEncrypt($key);
 
-$data = '{"source_uid": "123312", "source": "qq", "source_name": "hello"}';
-// 加密
-$data = pack("N",strlen($data)).$data;
-$string = $aes->encrypt($data);
-echo base64_encode($string);
-// 解密
-echo " ||  ",$aes->decrypt($string);
+// $data = '{"source_uid": "123312", "source": "qq", "source_name": "hello"}';
+// // 加密
+// $data = pack("N",strlen($data)).$data;
+// $string = $aes->encrypt($data);
+// echo base64_encode($string);
+// // 解密
+// echo " ||  ",$aes->decrypt($string);
 
 ?>
